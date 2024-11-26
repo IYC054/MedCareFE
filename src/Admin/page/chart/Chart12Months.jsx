043cdf41ddf1d16d2574214f0fcae06d651eceb0
unknown <tuann300724@gmail.com>
2024-11-26 12:15:47 +0700

    da

---
+++ src/Admin/page/chart/Chart12Months.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Chart from 'react-apexcharts';
+
+const Chart12Months = () => {
+    const data = {
+        series: [
+            {
+                name: "Earnings",
+                data: [12000, 15000, 10000, 20000, 18000, 22000, 30000, 25000, 27000, 23000, 19000, 24000],
+            },
+        ],
+        options: {
+            chart: { type: 'bar', height: 350 },
+            xaxis: { categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"] },
+            fill: {
+                gradient: {
+                    shade: 'light',
+                    type: 'vertical',
+                    shadeIntensity: 0.25,
+                    gradientToColors: ['#F7B924', '#FBDC92'], // Gradient màu
+                    inverseColors: false,
+                    opacityFrom: 0.65,
+                    opacityTo: 0.5,
+                },
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 5, // Làm tròn góc thanh bar
+                    horizontal: false, // Bar dọc
+                },
+            },
+            title: {
+                text: "Earnings by Month",
+                align: 'center',
+                style: {
+                    fontSize: '16px',
+                    color: '#333',
+                },
+            },
+        },
+    };
+
+    return (
+        <div className="widget-chart p-0">
+            <Chart
+                options={data.options}
+                series={data.series}
+                type="bar"
+                height="350"
+            />
+        </div>
+    );
+};
+
+export default Chart12Months;
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './page/RootLayout'
 import RootLayoutAdmin from './page/RootLayoutAdmin'
 import Dashboard from './Admin/page/Dashboard'
+import AccountUser from './Admin/page/AccountUser'
+import AccountDoctor from './Admin/page/AccountDoctor'
+import Appointment from './Admin/page/Appointment'
+import Feedback from './Admin/page/FeedBack'
+
 
 const router = createBrowserRouter([
   {
@@ -33,6 +38,22 @@ const router = createBrowserRouter([
       {
         path: "/admin",
         element: <Dashboard/>
+      },
+      {
+        path: "/admin/user",
+        element: <AccountUser/>
+      },
+      {
+        path: "/admin/doctor",
+        element: <AccountDoctor/>
+      },
+      {
+        path: "/admin/appointment",
+        element: <Appointment/>
+      },
+      {
+        path: "/admin/feedback",
+        element: <Feedback/>
       },
     ]
   }
