dc49ee6b044c2bc01cc2b890ab00967c9c25a420
nghimathit <vannghinguyen152001@gmail.com>
2024-11-27 16:59:17 +0700

    home

--- src/components/Home/Carosel.jsx
+++ src/components/Home/Carosel.jsx
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+const Carousel = () => {
+  const images = [
+    {
+      id: 1,
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9445a373-0da5-4c7c-997d-1e2058e59dbf-baby_dino_banner_1180x310_desktop.jpg&w=1200&q=100",
+    },
+    {
+      id: 2,
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F4edacddd-7280-41e0-b6c9-3eb7d438c107-banner_cashback_1180x310_desktop.jpg&w=1200&q=100",
+    },
+    {
+      id: 3,
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2Fe7eda662-3dd6-4b79-8253-baf031e6561e-promote-telemed-de.jpg&w=1200&q=100",
+    },
+    {
+      id: 4,
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2Fda64c9ee-fdd6-4a13-bc52-fe74255fc079-promote-vaccine-d.jpg&w=1200&q=100",
+    },
+    {
+      id: 5,
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2Fce76da74-5584-451b-b417-c3b68ce49cfa-viettel_money_banner_fb_1180x310_copy2_copy.png&w=1200&q=100",
+    },
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-const Carosel = () => {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [images.length]);
   return (
-    <div className="rounded-lg border-transparent border hover:border-[#00b5f1] hover:border w-28 ">
-      <div className="bg-white  rounded-lg ">
-        <div className="px-3 py-2 rounded-lg">
-          <img
-            className="mx-auto"
-            src="https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F94fad041-984a-4ed7-99e8-3a940360a1cc-7751fd3f-f46c-436a-af19-2c64d4d5cf25-dkcs.webp&w=64&q=75"
-            alt="Image"
-          />
-          <p>Đặt khám tại cơ sở</p>
-        </div>
+    <div>
+      <div className="rounded-lg h-full">
+        <img
+          src={images[currentIndex].img}
+          alt={`Slide ${currentIndex}`}
+          className="rounded-lg"
+        />
+        <div className="mt-1">
+        <ul className="flex gap-2 justify-center">
+        {images.map((item)=>{
+            return <li key={item.id} className={`cursor-pointer w-2 h-2 rounded-full ${currentIndex+1===item.id ?" bg-[#00b5f1]":" bg-gray-200"}`} onClick={()=>setCurrentIndex(item.id-1)}></li>
+        })}
+         
+        </ul>
       </div>
+      </div>
+      
     </div>
-   
-  
+    //viết thuần thật là phê
   );
 };
 
-export default Carosel;
+export default Carousel;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import Carosel from "../components/Home/carosel";
 import CustomCarousel from "../components/CustomCarousel";
+import Carousel from "../components/Home/carosel";
+import DoctorCarouselowl from "../components/doctorCarouselowl";
 
 const Home = () => {
   const ExaminationServices = [
-   
     {
       img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F94fad041-984a-4ed7-99e8-3a940360a1cc-7751fd3f-f46c-436a-af19-2c64d4d5cf25-dkcs.webp&w=64&q=75",
       text: "Đặt khám theo cơ sở",
@@ -36,44 +36,175 @@ const Home = () => {
     {
       img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fprod-partner.s3-hcm-r1.longvan.net%2F0640985d-4280-4e8c-8ec6-939f9a4cf44b-thanhtoanvp.png&w=64&q=75",
       text: "Thanh toán viện phí",
-    }
-    
+    },
   ];
   const logoHopital = [
-   
     {
       img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
       text: "Bệnh viện trung ương",
     },
     {
-      img: "",
-      text: "Đặt khám theo bác sĩ",
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
     },
     {
-      img: "",
-      text: "Tư vấn khám bệnh qua video",
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
     },
     {
-      img: "",
-      text: "Đặt lịch xét nghiệm",
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
     },
     {
-      img: "",
-      text: "Gói khám sức khỏe",
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
     },
     {
-      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn-pkh.longvan.net%2Fprod-partner%2Ff141b109-2daa-4953-ad55-5a395f900d46-tiaaam_chaaang.png&w=64&q=75",
-      text: "Đặt lịch tiêm chủng",
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
     },
     {
-      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn-pkh.longvan.net%2Fprod-partner%2Ffa0b00be-d554-404a-bf9a-4a5f216ee978-chaam_saac_taaoa_i_nhaa.png&w=64&q=75",
-      text: "Y tế tại nhà",
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
     },
     {
-      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fprod-partner.s3-hcm-r1.longvan.net%2F0640985d-4280-4e8c-8ec6-939f9a4cf44b-thanhtoanvp.png&w=64&q=75",
-      text: "Thanh toán viện phí",
-    }
-    
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
+    },
+    {
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
+    },
+    {
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
+    },
+    {
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
+    },
+    {
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
+    },
+    {
+      img: "https://medpro.vn/_next/image?url=https%3A%2F%2Fcdn.medpro.vn%2Fprod-partner%2F9e2176ed-db14-4603-a594-8cefb4c4a2d0-logo-circle-trung-vuong.png&w=64&q=75",
+      text: "Bệnh viện trung ương",
+    },
+  ];
+  const doctors = [
+    {
+      name: "BS. Lê Ngọc Hồng Hạnh",
+      specialty: "Nhi - Thần kinh",
+      price: "200.000",
+      rating: 4.2,
+      visits: 118,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Đức Bảo",
+      specialty: "Tai mũi họng",
+      price: "150.000",
+      rating: 4.2,
+      visits: 37,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "BS. Ngô Tài Dũng",
+      specialty: "Nhi khoa",
+      price: "250.000",
+      rating: 4.1,
+      visits: 51,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
+    {
+      name: "ThS. Nguyễn Thị Mỹ Linh",
+      specialty: "Cơ Xương Khớp",
+      price: "220.000",
+      rating: 4.1,
+      visits: 53,
+      avatar: "https://via.placeholder.com/80",
+    },
   ];
   const [placeholderText, setPlaceholderText] = useState("");
   const fullPlaceholders = [
@@ -157,17 +288,40 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="w-[80%] bg-red-600 h-96 mx-auto mt-16">
+      <div className="w-[80%] mx-auto mt-24">
         <div className="w-100">
-          <p className="font-bold text-xl text-center">ĐƯỢC SỰ TIN TƯỞNG HỢP TÁC VÀ ĐỒNG HÀNH</p>
+          <p className="font-bold text-xl text-center">
+            ĐƯỢC SỰ TIN TƯỞNG HỢP TÁC VÀ ĐỒNG HÀNH
+          </p>
           <div className="w-[70%] mx-auto gap-1 text-[12px] text-center font-semibold text-[#003553]">
             <div className="w-[100%] flex justify-center mx-auto items-center">
               <CustomCarousel items={logoHopital} w={"w-28"} />
             </div>
           </div>
         </div>
-
+        <div className="flex justify-center">
+          <Carousel />
+        </div>
+        <div className="mt-3">
+          <p className="text-center font-bold text-xl">
+            BÁC SĨ TƯ VẤN KHÁM QUA VIDEO
+          </p>
+          <DoctorCarouselowl items={doctors} w={"w-[216px]"} />
+          <div className="w-full flex justify-center mt-2">
+            <button class="inline-flex items-center justify-center bg-transparent border p-0 border-transparent hover:border-[#00b5f1] px-2 py-2 rounded-lg">
+              <p class="flex items-center m-0 text-[#00b5f1]">
+                Xem Tất Cả
+                <img
+                  src="https://medpro.vn/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdirection-left.35ddaa32.svg&w=48&q=75"
+                  alt="icon"
+                  class="ml-2 h-5"
+                />
+              </p>
+            </button>
+          </div>
+        </div>
       </div>
+
       <div>Nguywen van nghi</div>
       <div>Nguywen van nghi</div>
       <div>Nguywen van nghi</div>
