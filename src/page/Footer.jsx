721d8229c7b9b1dda082bc8f8678141491234817
nghimathit <vannghinguyen152001@gmail.com>
2024-11-28 10:58:08 +0700

    footer tam

--- src/page/Footer.jsx
+++ src/page/Footer.jsx
@@ -1,11 +1,101 @@
-import React from 'react';
+import React from "react";
 
-function Footer(props) {
-    return (
-        <div className='w-full h-screen bg-[#fff] shadow-xl'>
-            this is footer
+function Footer() {
+  return (
+    <div className="w-full shadow-xl">
+      <div
+        className="w-[80%]  mx-auto grid "
+        style={{ gridTemplateColumns: "40% 20% 20% 20%" }}
+      >
+        <div className=" font-light text-xs flex flex-col gap-2 px-1 lg:px-24">
+          <img
+            src="https://medpro.vn/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.d646d4b8.svg&w=750&q=75"
+            alt="Logo"
+            className="w-32"
+          />
+          <p>
+            <span className="font-bold">Địa chỉ:</span> 236/29/18 Điện Biên Phủ
+            - Phường 17 - Quận Bình Thạnh - TPHCM.
+          </p>
+          <p>
+            <span className="font-bold">Website:</span> https://medpro.vn
+          </p>
+          <p>
+            <span className="font-bold">Email:</span> cskh@medpro.vn
+          </p>
+          <p>
+            <span className="font-bold">Điện thoại:</span> (028) 710 78098
+          </p>
         </div>
-    );
+
+        <div className=" font-light text-xs flex flex-col gap-1 px-4">
+          <p className="font-bold">Dịch vụ Y tế</p>
+          <p>Đặt khám tại cơ sở</p>
+          <p>Đặt khám theo bác sĩ</p>
+          <p>Tư vấn khám bệnh qua video</p>
+          <p>Đặt lịch xét nghiệm</p>
+          <p>Gói khám sức khỏe</p>
+          <p>Đặt lịch tiêm chủng</p>
+          <p>Y tế tại nhà</p>
+          <p>Thanh toán Viện phí</p>
+          <p className="font-bold mt-2">Dịch vụ Y tế</p>
+          <p>Đặt khám tại cơ sở</p>
+          <p>Đặt khám theo bác sĩ</p>
+          <p>Tư vấn khám bệnh qua video</p>
+          <p>Đặt lịch xét nghiệm</p>
+          <p>Gói khám sức khỏe</p>
+          <p>Đặt lịch tiêm chủng</p>
+          <p>Y tế tại nhà</p>
+          <p>Thanh toán Viện phí</p>
+        </div>
+        <div className=" font-light text-xs flex flex-col gap-1 px-4">
+          <p className="font-bold">Dịch vụ Y tế</p>
+          <p>Đặt khám tại cơ sở</p>
+          <p>Đặt khám theo bác sĩ</p>
+          <p>Tư vấn khám bệnh qua video</p>
+          <p>Đặt lịch xét nghiệm</p>
+          <p>Gói khám sức khỏe</p>
+          <p>Đặt lịch tiêm chủng</p>
+          <p>Y tế tại nhà</p>
+          <p>Thanh toán Viện phí</p>
+          <p className="font-bold mt-2">Dịch vụ Y tế</p>
+          <p>Đặt khám tại cơ sở</p>
+          <p>Đặt khám theo bác sĩ</p>
+          <p>Tư vấn khám bệnh qua video</p>
+          <p>Đặt lịch xét nghiệm</p>
+          <p>Gói khám sức khỏe</p>
+          <p>Đặt lịch tiêm chủng</p>
+          <p>Y tế tại nhà</p>
+          <p>Thanh toán Viện phí</p>
+        </div>
+        <div className=" font-light text-xs flex flex-col gap-1 px-4">
+          <p className="font-bold">Dịch vụ Y tế</p>
+          <p>Đặt khám tại cơ sở</p>
+          <p>Đặt khám theo bác sĩ</p>
+          <p>Tư vấn khám bệnh qua video</p>
+          <p>Đặt lịch xét nghiệm</p>
+          <p>Gói khám sức khỏe</p>
+          <p>Đặt lịch tiêm chủng</p>
+          <p>Y tế tại nhà</p>
+          <p>Thanh toán Viện phí</p>
+          <p className="font-bold mt-2">Dịch vụ Y tế</p>
+          <p>Đặt khám tại cơ sở</p>
+          <p>Đặt khám theo bác sĩ</p>
+          <p>Tư vấn khám bệnh qua video</p>
+          <p>Đặt lịch xét nghiệm</p>
+          <p>Gói khám sức khỏe</p>
+          <p>Đặt lịch tiêm chủng</p>
+          <p>Y tế tại nhà</p>
+          <p>Thanh toán Viện phí</p>
+        </div>
+      </div>
+      <div className="bg-[linear-gradient(82.98deg,_#00b5f1_33.22%,_#00e0ff_135.46%)] mt-1">
+        <p className="text-center py-2 text-[12px] font-normal text-white">© 2020 - Bản quyền thuộc Công Ty Cổ Phần Ứng Dụng PKH</p>
+      </div>
+
+      <p></p>
+    </div>
+  );
 }
 
 export default Footer;
