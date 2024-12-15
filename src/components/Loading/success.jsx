7d43f002e66938540b71efdb7226f2b04c7c3f1f
thanh phong <vonghung849@gmail.com>
2024-12-15 10:24:31 +0700

    fix

---
+++ src/components/Loading/success.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './success.scss'
+function Success(props) {
+  return (
+    <div className="box">
+      <div className="success alert">
+      </div>
+    </div>
+  );
+}
+
+export default Success;
