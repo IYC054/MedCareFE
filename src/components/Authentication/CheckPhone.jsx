4dd740fda04de3124764a72602cd60c257fdfb53
IYC <trieunhananh@gmail.com>
2024-12-29 23:40:35 +0700

    iyc fix

--- src/components/Authentication/CheckPhone.jsx
+++ src/components/Authentication/CheckPhone.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 import ImgAuth from "../../../asset/auth_img.png";
+import Login from "./Login";
+import { useEffect, useState, memo, useContext } from "react";
 
 function CheckPhone({close}) {
+  const [next, setNext] = useState(false);
+
+  const handleNext = () => {
+    setNext(!next);
+  };
   return (
     //  <div className="bg-white rounded-lg p-6 shadow-lg w-9/12 h-4/5 z-50">
     //  <div className="flex h-full items-center justify-center bg-gray-100 ">
       <div className="bg-white rounded-lg shadow-lg flex w-4/5 max-w-screen-lg overflow-hidden">
-     
-        {/* Form bên trái */}
-        <div className="flex-1 p-8">
-          <h1 className="text-4xl font-bold text-blue-600 mb-6">medpro</h1>
-          <p className="mb-4 text-gray-600">Vui lòng nhập email để tiếp tục</p>
-          <div className="flex items-center border border-gray-300 rounded-md overflow-hidden mb-4">
-            {/* <span className="px-3 bg-gray-100 text-gray-700">🇻🇳 +84</span> */}
-            <input
-              type="text"
-              placeholder="Nhập email"
-              className="flex-1 px-4 py-2 focus:outline-none"
-            />
-          </div>
-          <button className="w-full py-2 bg-blue-600 text-white rounded-md mb-4 hover:bg-blue-700">
-            Tiếp tục
-          </button>
-          <div className="text-center text-gray-500 mb-4">Hoặc đăng nhập bằng tài khoản</div>
-          <button className="w-full py-2 bg-red-600 text-white rounded-md mb-4 hover:bg-red-700">
-            Đăng nhập với Google
-          </button>
-          <button className="w-full py-2 bg-blue-800 text-white rounded-md hover:bg-blue-900">
-            Đăng nhập với Facebook
-          </button>
-        </div>
+     {!next && (
+      <div className="flex-1 p-8">
+      <h1 className="text-4xl font-bold text-blue-600 mb-6">medpro</h1>
+      <p className="mb-4 text-gray-600">Vui lòng nhập email để tiếp tục</p>
+      <div className="flex items-center border border-gray-300 rounded-md overflow-hidden mb-4">
+        {/* <span className="px-3 bg-gray-100 text-gray-700">🇻🇳 +84</span> */}
+        <input
+          type="text"
+          placeholder="Nhập email"
+          className="flex-1 px-4 py-2 focus:outline-none"
+        />
+      </div>
+      <button className="w-full py-2 bg-blue-600 text-white rounded-md mb-4 hover:bg-blue-700" onClick={handleNext}>
+        Tiếp tục
+      </button>
+      <div className="text-center text-gray-500 mb-4">Hoặc đăng nhập bằng tài khoản</div>
+      <button className="w-full py-2 bg-red-600 text-white rounded-md mb-4 hover:bg-red-700">
+        Đăng nhập với Google
+      </button>
+      <button className="w-full py-2 bg-blue-800 text-white rounded-md hover:bg-blue-900">
+        Đăng nhập với Facebook
+      </button>
+    </div>
+     )}
+     {next && <Login/>}
+        
+        
 
         {/* Hình ảnh bên phải */}
         
--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
-
-function Login(props) {
+import { useEffect, useState, memo, useContext } from "react";
+const Login = () => {
     return (
-        <div>
-            
-        </div>
-    );
+        <div className="flex-1 p-8">
+      <h1 className="text-4xl font-bold text-blue-600 mb-6">medpro</h1>
+      <p className="mb-4 text-gray-600">Vui lòng nhập email để tiếp tục</p>
+      <form>
+      <div className="flex items-center border border-gray-300 rounded-md overflow-hidden mb-4">
+        {/* <span className="px-3 bg-gray-100 text-gray-700">🇻🇳 +84</span> */}
+        <input
+          type="text"
+          placeholder="Nhập email"
+          className="flex-1 px-4 py-2 focus:outline-none"
+        />
+      </div>
+      <div className="flex items-center border border-gray-300 rounded-md overflow-hidden mb-4">
+        {/* <span className="px-3 bg-gray-100 text-gray-700">🇻🇳 +84</span> */}
+        <input
+                type="password"
+                placeholder="Nhập mật khẩu"
+                className="flex-1 px-4 py-2 focus:outline-none"
+              />
+      </div>
+      <button className="w-full py-2 bg-blue-600 text-white rounded-md mb-4 hover:bg-blue-700" >
+        Đăng nhập
+      </button>
+      </form>
+      <span className="text-[rgb(117,145,244)] text-sm cursor-pointer">Quên mật khẩu ?</span>
+    </div>
+      );
 }
 
 export default Login;
